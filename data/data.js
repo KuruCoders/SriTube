0a778d5fc514c8b217422bf7c7dd7ed8cce1839b
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-12-24 23:09:36 +0530

    AddedTailwind

---
+++ data/data.js
@@ -0,0 +1,126 @@
+export const categories = [
+    'All',
+    'Gaming',
+    'React Routers',
+    'Music',
+    'John Snow',
+    'Live',
+    'Gadgets',
+    'Programming',
+    'Podcasts',
+    'Computers',
+    'Others'
+]
+
+export const shortVideos = [
+    {
+        id: 2,
+        title: 'Reasons you should learn React Native',
+        viewCount: '355k',
+        image: require('../assets/images/short2.png')
+    },
+    {
+        id: 2,
+        title: "Mobile app development",
+        viewCount: '132k',
+        image: require('../assets/images/short5.png')
+    },
+    {
+        id: 3,
+        title: "Tesla's Wireless Charger: Explained!",
+        viewCount: '2M',
+        image: require('../assets/images/short3.png')
+    },
+    {
+        id: 4,
+        title: "Rare Moments in Football 🥶🤩",
+        viewCount: '167k',
+        image: require('../assets/images/short4.png')
+    },
+    {
+        id: 5,
+        title: 'Fastest Way to Learn React in 2023 🔥',
+        viewCount: '1.2M',
+        image: require('../assets/images/short1.png')
+    },
+    {
+        id: 6,
+        title: 'උමංදාව and කොම්පැනි #umandawa',
+        viewCount: '820k',
+        image: require('../assets/images/short6.png')
+    },
+]
+
+export const videos = [
+    {
+        id: 1,
+        title: 'Homeless Man Buys A Lamborghini',
+        channelTitle: 'MrBeast',
+        thumbnail: require('../assets/images/thumb1.png'),
+        avatar: require('../assets/images/avatar1.png'),
+        viewCount: '130M',
+        duration: '14:50',
+        publishedText: '2 years ago'
+    },
+    {
+        id: 2,
+        title: 'I Spent 30 Days Freelancing on Upwork',
+        channelTitle: 'Kenny Gunderman',
+        thumbnail: require('../assets/images/thumb2.png'),
+        avatar: require('../assets/images/avatar2.png'),
+        viewCount: '1M',
+        duration: '4:30',
+        publishedText: '5 months ago'
+    },
+    {
+        id: 3,
+        title: 'උමංදාව and චොකලට් කොම්පැනි #umandawa',
+        channelTitle: 'Siri Sadaham Ashramaya',
+        thumbnail: require('../assets/images/thumb6.png'),
+        avatar: require('../assets/images/avatar6.jpg'),
+        viewCount: '82K',
+        duration: '11:22',
+        publishedText: '6 days ago'
+    },
+    {
+        id: 4,
+        title: 'මාව සතෙක් කරගන්ඩෙපා !! | The kota',
+        channelTitle: 'The Kota',
+        thumbnail: require('../assets/images/thumb6.png'),
+        avatar: require('../assets/images/avatar6.jpg'),
+        viewCount: '239K',
+        duration: '20:59',
+        publishedText: '2 weeks ago'
+    },
+    {
+        id: 5,
+        title: '6 Of My Personal Tips When Learning To Code',
+        channelTitle: 'Traversy Media',
+        thumbnail: require('../assets/images/thumb3.png'),
+        avatar: require('../assets/images/avatar3.png'),
+        viewCount: '151k',
+        duration: '6:00',
+        publishedText: '1 year ago'
+    },
+    {
+        id: 6,
+        title: 'Top 10 CSS One Liners That Will Blow Your Mind',
+        channelTitle: 'developedbyed',
+        thumbnail: require('../assets/images/thumb4.png'),
+        avatar: require('../assets/images/avatar4.png'),
+        viewCount: '400k',
+        duration: '7:45',
+        publishedText: '1 Month ago'
+    },
+    {
+        id: 7,
+        title: 'Fruit Shop UI - React Native Tutorial',
+        channelTitle: 'Code With Nomi',
+        thumbnail: require('../assets/images/thumb5.png'),
+        avatar: require('../assets/images/avatar5.png'),
+        viewCount: '5K',
+        duration: '20:45',
+        publishedText: '11 days ago'
+    },
+
+]
